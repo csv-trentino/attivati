830962188206a4599bd4e421d2a386d9903ec8e3
Fabrizio Arzeni <fabrizio.arz@gmail.com>
2024-10-19 18:45:07 +0200

    fix: append /api to endpoints url

--- volunteer-app/src/config/network.ts
+++ volunteer-app/src/config/network.ts
@@ -1,8 +1,8 @@
 export function getApiUrl(): string {
-  let baseUrl = process.env.EXPO_PUBLIC_API_URL;
+  let baseUrl = process.env.EXPO_PUBLIC_API_URL + "/api";
 
   if (__DEV__) {
-    baseUrl = process.env.EXPO_PUBLIC_DEV_API_URL;
+    baseUrl = process.env.EXPO_PUBLIC_DEV_API_URL + "/api";
   }
   if (!baseUrl) {
     throw new Error("API_BASE_URL is not set");
