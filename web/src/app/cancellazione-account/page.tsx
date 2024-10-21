6fc158951142a735a0eab2447feab5aa30bef01d
Fabrizio Arzeni <fabrizio.arz@gmail.com>
2024-10-21 16:28:10 +0200

    feat: delete account page

---
+++ web/src/app/cancellazione-account/page.tsx
@@ -0,0 +1,32 @@
+
+
+export default function PrivacyPolicy() {
+  return (
+    <div className="prose my-12 px-4 lg:px-10 max-w-screen-lg">
+      <h1 className="mb-0 font-semibold">Cancellazione account</h1>
+
+      <p>
+        Per richiedere la cancellazione del tuo account, procedi come segue:
+
+      </p>
+      <ul>
+        <li>Accedi con al tuo account utilizzando la app Attivati! sul tuo smartphone</li>
+
+        <li>Procedi sulla sezione &quot;Impostazioni&quot;</li>
+        <li>Seleziona la voce &quot;Account&quot;</li>
+        <li>Seleziona &quot;Cancella account&quot;</li>
+        <li>Conferma la tua volontà di cancellare il tuo account</li>
+        <li>Inserisci il codice a 5 cifre che ti verrà inviato via email</li>
+      </ul>
+
+      <p>
+        Verranno cancellati tutti i dati relativi al tuo account e non sarà più possibile accedere al servizio.
+
+      </p>
+
+      <p>
+        Sarà possibile creare un nuovo account in qualsiasi momento, ma i dati relativi al vecchio account non saranno più disponibili.
+      </p>
+    </div>
+  )
+}
