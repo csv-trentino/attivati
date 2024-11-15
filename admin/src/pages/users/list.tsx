8b4923f9f724e2451295df39f9dd89668c3b65e9
Jonathan Mataloni <jo.mataloni@gmail.com>
2024-11-15 16:01:41 +0100

    feat(adm): export users button

--- admin/src/pages/users/list.tsx
+++ admin/src/pages/users/list.tsx
@@ -1,15 +1,20 @@
 import {
   BooleanField,
+  CreateButton,
   DeleteButton,
   EditButton,
+  ExportButton,
   List,
   ShowButton,
   useTable,
 } from "@refinedev/antd";
-import { BaseRecord } from "@refinedev/core";
+import { BaseRecord, useGo } from "@refinedev/core";
 import { Space, Table } from "antd";
+import { getApiEndpoint, getAxiosInstance } from "../../config/network";
 
 export const UserList = () => {
+  const go = useGo();
+
   const { tableProps } = useTable({
     syncWithLocation: true,
   });
@@ -17,8 +22,41 @@ export const UserList = () => {
   const TrueIcon = () => <span>✅</span>;
   const FalseIcon = () => <span>❌</span>;
 
+  const handleExport = () => {
+    const client = getAxiosInstance();
+
+    client
+      .get(`${getApiEndpoint()}/user-export`, {
+        responseType: "blob",
+      })
+      .then((res) => {
+        const href = URL.createObjectURL(res.data);
+
+        const link = document.createElement("a");
+        link.href = href;
+        link.setAttribute("download", "users.csv"); 
+        document.body.appendChild(link);
+        link.click();
+
+        document.body.removeChild(link);
+        URL.revokeObjectURL(href);
+      })
+      .catch((error) => {
+        console.error("Error exporting users", error);
+      });
+  };
+
   return (
-    <List>
+    <List
+      headerProps={{
+        extra: (
+          <Space>
+            <ExportButton onClick={handleExport} />
+            <CreateButton onClick={() => go({ to: "/users/create" })} />
+          </Space>
+        ),
+      }}
+    >
       <Table {...tableProps} rowKey="id">
         <Table.Column dataIndex="id" title={"ID"} />
         <Table.Column dataIndex="first_name" title={"Name"} />
